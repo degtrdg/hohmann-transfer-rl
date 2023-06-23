118426513a3785c2249269694e42bf1fa8c916c7
Daniel George <degtrdg@github.com>
2023-06-23 14:49:08 -0500

    Euler step method

--- HohmannTransferEnv.py
+++ HohmannTransferEnv.py
@@ -10,17 +10,14 @@ class HohmannTransferEnv(gym.Env):
     Observation: 
         Type: Box(5)
         Num     Observation             Min    Max
-        0       Spaceship position x    -Inf   Inf
-        1       Spaceship position y    -Inf   Inf
-        2       Spaceship velocity x    -Inf   Inf
-        3       Spaceship velocity y    -Inf   Inf
-        4       Gravitational force     0      Inf
+        0       Spaceship position      -Inf   Inf
+        1       Spaceship velocity      -Inf   Inf
+        2       Gravitational force     0      Inf
 
     Actions:
         Type: Box(2)
         Num   Action
-        0     Thrust in x direction
-        1     Thrust in y direction
+        0     Thrust vector
         
     Note: The spaceship is considered a point mass.
     """
@@ -32,8 +29,8 @@ class HohmannTransferEnv(gym.Env):
         self.earth_mass = 5.972e24  # in kg
         self.spaceship_mass = 1000  # in kg, just an assumption
 
-        self.action_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(2,), dtype=np.float32)
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(5,), dtype=np.float32)
+        self.action_space = spaces.Box(low=self.min_action, high=self.max_action, shape=(1,), dtype=np.float32)
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float32)
 
         self.state = None  # will be initialized in reset method
 
@@ -45,44 +42,51 @@ class HohmannTransferEnv(gym.Env):
         # Limit the action space
         action = np.clip(action, self.action_space.low, self.action_space.high)
 
+        # Seperate position, velocity, and force
+        pos = self.state[0]
+        vel = self.state[1]
+        Fg = self.state[2]
+
+        a = Fg / self.spaceship_mass + action
+
+        t = 0.1
+        vel_ = vel + a*t
+        pos_ = pos + vel*t + 0.5*a*t**2
+        Fg_ = self.gravity_constant * self.earth_mass * self.spaceship_mass / np.linalg.norm(pos_)**2
+
+        self.state = np.array([pos_, vel_, Fg_])
+
+        return self.state, 1, False, {}
+
         # Define the ODE
-        def ode(t, y):
-            # What do each of the elements of y represent?
-            # y[0] = x
-            # y[1] = y
-            # y[2] = vx
-            # y[3] = vy
-            # y[4] = Fg
-            r = np.sqrt(y[0]**2 + y[1]**2)
-            return [y[2],  # dx/dt = vx
-                    y[3],  # dy/dt = vy
-                    -self.gravity_constant * self.earth_mass / r**3 * y[0] + action[0]/self.spaceship_mass,  # dvx/dt = ax
-                    -self.gravity_constant * self.earth_mass / r**3 * y[1] + action[1]/self.spaceship_mass,  # dvy/dt = ay
-                    self.gravity_constant * self.earth_mass / r**2]  # dFg/dt = 0
+        # def ode(t, y):
+        #     dy, v = y
+        #     Fg = self.gravity_constant * self.earth_mass * self.spaceship_mass / np.linalg.norm(dy)**2
+        #     return [v, Fg * dy / (np.linalg.norm(dy)*self.spaceship_mass) + action]
 
-        # Define the time span of the integration
-        t_span = [0, 1]  # 1-second step
+        # # Define the time span of the integration
+        # t_span = [0, 1]  # 1-second step
 
-        # Solve the ODE
-        sol = solve_ivp(ode, t_span, self.state)
+        # # Solve the ODE
+        # sol = solve_ivp(ode, t_span, self.state)
 
-        # The new state is the final value of the solution
-        next_state = sol.y[:, -1]
+        # # The new state is the final value of the solution
+        # next_state = sol.y[:, -1]
 
-        # Define the reward function
-        reward = 1  # placeholder for the reward function
+        # # Define the reward function
+        # reward = 1  # placeholder for the reward function
 
-        # Define the terminal condition
-        r = np.sqrt(next_state[0]**2 + next_state[1]**2)
-        done = r <= 10 or r >= 100  # The episode ends if the spaceship is within 10 units from the center of the Earth or more than 100 units away.
+        # # Define the terminal condition
+        # r = np.sqrt(next_state[0]**2 + next_state[1]**2)
+        # done = r <= 10 or r >= 100  # The episode ends if the spaceship is within 10 units from the center of the Earth or more than 100 units away.
 
-        self.state = next_state
-        return next_state, reward, done, {}
+        # self.state = next_state
+        # return next_state, reward, done, {}
 
     def reset(self):
         # Define the initial state here.
-        # The state vector is: [x, y, vx, vy, Fg]
-        self.state = np.array([0., 0., 0., 0., self.gravity_constant * self.earth_mass])
+        # The state vector is: [[x, y], [vx, vy], Fg]
+        self.state = np.array([[0,0], [0,0], [0,0]])
         return self.state
 
 
