f1cfb2c2c75335bb54c6c897a95c93920a41a3c3
Daniel George <degtrdg@github.com>
2023-06-23 13:27:51 -0500

    Prelim ODE solver

--- HohmannTransferEnv.py
+++ HohmannTransferEnv.py
@@ -41,12 +41,50 @@ class HohmannTransferEnv(gym.Env):
         # Implement the dynamics of the environment here.
         # Use scipy's solve_ivp to get the new state given the current state and action.
         # Remember to respect the constraints of the environment.
-        pass
+
+        # Limit the action space
+        action = np.clip(action, self.action_space.low, self.action_space.high)
+
+        # Define the ODE
+        def ode(t, y):
+            # What do each of the elements of y represent?
+            # y[0] = x
+            # y[1] = y
+            # y[2] = vx
+            # y[3] = vy
+            # y[4] = Fg
+            r = np.sqrt(y[0]**2 + y[1]**2)
+            return [y[2],  # dx/dt = vx
+                    y[3],  # dy/dt = vy
+                    -self.gravity_constant * self.earth_mass / r**3 * y[0] + action[0]/self.spaceship_mass,  # dvx/dt = ax
+                    -self.gravity_constant * self.earth_mass / r**3 * y[1] + action[1]/self.spaceship_mass,  # dvy/dt = ay
+                    self.gravity_constant * self.earth_mass / r**2]  # dFg/dt = 0
+
+        # Define the time span of the integration
+        t_span = [0, 1]  # 1-second step
+
+        # Solve the ODE
+        sol = solve_ivp(ode, t_span, self.state)
+
+        # The new state is the final value of the solution
+        next_state = sol.y[:, -1]
+
+        # Define the reward function
+        reward = 1  # placeholder for the reward function
+
+        # Define the terminal condition
+        r = np.sqrt(next_state[0]**2 + next_state[1]**2)
+        done = r <= 10 or r >= 100  # The episode ends if the spaceship is within 10 units from the center of the Earth or more than 100 units away.
+
+        self.state = next_state
+        return next_state, reward, done, {}
 
     def reset(self):
         # Define the initial state here.
         # The state vector is: [x, y, vx, vy, Fg]
-        pass
+        self.state = np.array([0., 0., 0., 0., self.gravity_constant * self.earth_mass])
+        return self.state
+
 
     def render(self, mode='human'):
         pass
